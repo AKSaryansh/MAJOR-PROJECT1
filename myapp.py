7342f6003d8ffb9c8b85fbb65ce099965d264777
AKSaryansh <109017517+AKSaryansh@users.noreply.github.com>
2022-07-15 16:33:35 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,17 +2,16 @@ import streamlit as st
 import joblib
 model = joblib.load('ADMISSION PREDICTION')
 st.title('WILL YOU BE GET ADMITTED? ENTER THE DETAILS BELOW TO CHECK YOUR CHANCES!')
-list = ['GRE Score','TOEFL Score','University Rating','SOP','LOR','CGPA','RESEARCH PROGRESS(Answer in 0 or 1)']
+list = ['GRE Score','TOEFL Score','University Rating','SOP','LOR','CGPA','RESEARCH PROGRESS(Answer in 0 or 1)','end']
 list_for_input = [1,2,3,4,5,6,7]
 if st.button('Start'):
   i = 0
   while i<7:
     
     list_for_input[i] = st.number_input(f'Enter your {list[i]}')
-    if st.button('NEXT'):
+    if st.button(f'LET US MOVE TO {list[i+1]}'):
       i +=1
       
-  
 op = model.predict([list_for_input])
 if st.button('SUBMIT!'):
   st.title(str(op[0]*100) + '%')
