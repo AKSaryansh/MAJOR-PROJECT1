5fc9c24c9ee43bed06b6f4b8021418c002291650
AKSaryansh <109017517+AKSaryansh@users.noreply.github.com>
2022-07-15 16:13:37 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,11 +3,11 @@ import joblib
 model = joblib.load('ADMISSION PREDICTION')
 st.title('WILL YOU BE GET ADMITTED? ENTER THE DETAILS BELOW TO CHECK YOUR CHANCES!')
 list = ['GRE Score','TOEFL Score','University Rating','SOP','LOR','CGPA','RESEARCH PROGRESS(Answer in 0 or 1)']
-list_for_input = []
+list_for_input = [1,2,3,4,5,6,7]
 if st.button('Start'):
   for i in range(0,7):
-      st.number_input(f'Enter your {list[i]}')
-      list_for_input.append(st.number_input(f'Enter your {list[i]}'))
+      list_for_input[i] = st.number_input(f'Enter your {list[i]}')
+      
   
 op = model.predict([list_for_input])
 if st.button('SUBMIT!'):
