8294706e423b86081c184c15e81f25160c22dc74
AKSaryansh <109017517+AKSaryansh@users.noreply.github.com>
2022-07-16 00:39:44 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,25 +2,20 @@ import streamlit as st
 import joblib
 model = joblib.load('ADMISSION PREDICTION')
 st.title('WILL YOU BE GET ADMITTED? ENTER THE DETAILS BELOW TO CHECK YOUR CHANCES!')
-list = ['GRE Score','TOEFL Score','University Rating','SOP','LOR','CGPA','RESEARCH PROGRESS(Answer in 0 or 1)','end']
-list = []
-def f(a1,a2,a3,a4,a5,a6,a7):
-  a1 = st.number_input('Enter',key = 0)
-  if st.button('Next',key = 0):
-    a2 = st.number_input('Enter',key = 1)
-    if st.button('Next',key = 1):
-      a3 = st.number_input('Enter',key = 2)
-      if st.button('Next',key = 2):
-        a4 = st.number_input('Enter',key = 3)
-        if st.button('Next',key = 3):
-          a5 = st.number_input('Enter',key = 4)
-          if st.button('Next',key = 4):
-            a6 = st.number_input('Enter',key = 5)
-            if st.button('Next',key = 5):
-              a7 = st.number_input('Enter',key = 6)
-st.button('Start',on_click = f(a1,a2,a3,a4,a5,a6,a7))
+if st.button('Start'):
+  a1 = st.number_input('Enter your GRE Score',key = 0)
+  a2 = st.number_input('Enter your TOEFL Score',key = 1)
+  a3 = st.number_input('Enter your University Rating',key = 2)
+  a4 = st.number_input('Enter your SOP',key = 3)
+  a5 = st.number_input('Enter your LOR',key = 4)
+  a6 = st.number_input('Enter your CGPA',key = 5)
+  a7 = st.number_input('Enter your RESEARCH PROGRESS(Answer in 0 or 1)',key = 6)
+
 
 op = model.predict([[a1,a2,a3,a4,a5,a6,a7]])
 if st.button('SUBMIT!'):
-  st.title(str(op[0]*100) + '%')
+  if op[0] < 0:
+    st.title('Please Enter Appropriate Values!')
+  else:
+    st.title(str(op[0]*100) + '%')
       
