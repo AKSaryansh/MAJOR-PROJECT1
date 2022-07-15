eae5da2b1c104cf710d44f1ddedb6a7186685972
AKSaryansh <109017517+AKSaryansh@users.noreply.github.com>
2022-07-16 00:21:33 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,7 +3,8 @@ import joblib
 model = joblib.load('ADMISSION PREDICTION')
 st.title('WILL YOU BE GET ADMITTED? ENTER THE DETAILS BELOW TO CHECK YOUR CHANCES!')
 list = ['GRE Score','TOEFL Score','University Rating','SOP','LOR','CGPA','RESEARCH PROGRESS(Answer in 0 or 1)','end']
-if st.button('Start'):
+list = []
+def f(a1,a2,a3,a4,a5,a6,a7):
   a1 = st.number_input('Enter',key = 0)
   if st.button('Next',key = 0):
     a2 = st.number_input('Enter',key = 1)
@@ -17,8 +18,7 @@ if st.button('Start'):
             a6 = st.number_input('Enter',key = 5)
             if st.button('Next',key = 5):
               a7 = st.number_input('Enter',key = 6)
-      
-    
+st.button('Start',on_click = f(a1,a2,a3,a4,a5,a6,a7))
 
 op = model.predict([[a1,a2,a3,a4,a5,a6,a7]])
 if st.button('SUBMIT!'):
