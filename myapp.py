9da9b2e72532d2be03238ee5407e4287d23590da
AKSaryansh <109017517+AKSaryansh@users.noreply.github.com>
2022-07-16 00:45:09 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,13 +3,13 @@ import joblib
 model = joblib.load('ADMISSION PREDICTION')
 st.title('WILL YOU BE GET ADMITTED? ENTER THE DETAILS BELOW TO CHECK YOUR CHANCES!')
 if st.button('Start'):
-  a1 = st.number_input('Enter your GRE Score',key = 0)
-  a2 = st.number_input('Enter your TOEFL Score',key = 1)
-  a3 = st.number_input('Enter your University Rating',key = 2)
-  a4 = st.number_input('Enter your SOP',key = 3)
-  a5 = st.number_input('Enter your LOR',key = 4)
-  a6 = st.number_input('Enter your CGPA',key = 5)
-  a7 = st.number_input('Enter your RESEARCH PROGRESS(Answer in 0 or 1)',key = 6)
+  a1 = st.number_input('Enter your GRE Score')
+  a2 = st.number_input('Enter your TOEFL Score')
+  a3 = st.number_input('Enter your University Rating')
+  a4 = st.number_input('Enter your SOP')
+  a5 = st.number_input('Enter your LOR')
+  a6 = st.number_input('Enter your CGPA')
+  a7 = st.number_input('Enter your RESEARCH PROGRESS(Answer in 0 or 1)')
 
 
 op = model.predict([[a1,a2,a3,a4,a5,a6,a7]])
