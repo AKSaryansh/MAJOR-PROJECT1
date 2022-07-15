adc42bf80396fb2700e601dd814edc1401b1d2cd
AKSaryansh <109017517+AKSaryansh@users.noreply.github.com>
2022-07-16 00:03:15 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,19 +4,19 @@ model = joblib.load('ADMISSION PREDICTION')
 st.title('WILL YOU BE GET ADMITTED? ENTER THE DETAILS BELOW TO CHECK YOUR CHANCES!')
 list = ['GRE Score','TOEFL Score','University Rating','SOP','LOR','CGPA','RESEARCH PROGRESS(Answer in 0 or 1)','end']
 if st.button('Start'):
-  a1 = number_input('Enter',key = 0)
+  a1 = st.number_input('Enter',key = 0)
   if st.button('Next',key = 0):
-    a2 = number_input('Enter',key = 1)
+    a2 = st.number_input('Enter',key = 1)
     if st.button('Next',key = 1):
-      a3 = number_input('Enter',key = 2)
+      a3 = st.number_input('Enter',key = 2)
       if st.button('Next',key = 2):
-        a4 = number_input('Enter',key = 3)
+        a4 = st.number_input('Enter',key = 3)
         if st.button('Next',key = 3):
-          a5 = number_input('Enter',key = 4)
+          a5 = st.number_input('Enter',key = 4)
           if st.button('Next',key = 4):
-            a6 = number_input('Enter',key = 5)
+            a6 = st.number_input('Enter',key = 5)
             if st.button('Next',key = 5):
-              a7 = number_input('Enter',key = 6)
+              a7 = st.number_input('Enter',key = 6)
       
     
 
